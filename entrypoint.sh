482dc5d9bd2b6f6a2caf5c7345c82e3ef0e5d8e5
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2016-04-15 11:59:53 +0200

    Document entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+
 set -e
+
+# Configure Simian Army
 confd -onetime ${CONFD_OPTS:-"-backend=env"}
+
+# Execute any debugging commands, e.g. "/bin/bash" to get a login shell
 if test $# -ge 1; then
     exec "$@"
 fi
+
+# Start Simian Army
 exec ./gradlew jettyRun
